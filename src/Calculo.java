4ba1db6b290bef64219f82538daa46fc1ca6d3cf
Mikaela Guedes <mikaelaperezguedes@gmail.com>
2023-07-14 14:22:56 -0300

    commit1

---
+++ src/Calculo.java
@@ -0,0 +1,11 @@
+public class Calculo {
+    public static void main(String[] args) throws Exception {
+        int nota1 = 2;
+        int nota2 = 4;
+        int nota3 = 6;
+        int nota4 = 8;
+
+        int media = (nota1+nota2+nota3+nota4)/4; 
+        System.out.println("A média das notas é: " + media);
+    }
+}
